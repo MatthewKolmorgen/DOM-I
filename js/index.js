b908544885dd26107de35ae7182b276eb4dfc1e7
Matthew <mattk.development@gmail.com>
2020-08-03 13:56:17 -0500

    Finished Nav-Bar

--- js/index.js
+++ js/index.js
@@ -1,30 +1,51 @@
 const siteContent = {
   "nav": {
     "nav-item-1": "Services",
+    
     "nav-item-2": "Product",
+
+
     "nav-item-3": "Vision",
+
     "nav-item-4": "Features",
+
     "nav-item-5": "About",
+
     "nav-item-6": "Contact",
+
     "img-src": "img/logo.png"
   },
   "cta": {
     "h1": "DOM Is Awesome",
+
     "button": "Get Started",
+
     "img-src": "img/header-img.png"
   },
   "main-content": {
+
     "features-h4":"Features",
+
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
     "about-h4":"About",
+
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
     "middle-img-src": "img/mid-page-accent.jpg",
+
     "services-h4":"Services",
+
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
     "product-h4":"Product",
+
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
     "vision-h4":"Vision",
+
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+
   },
   "contact": {
     "contact-h4" : "Contact",
@@ -39,4 +60,17 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+const navItem = document.querySelectorAll("nav a");
+navItem[0].textContent = siteContent["nav"]["nav-item-1"];
+navItem[1].textContent = siteContent["nav"]["nav-item-2"];
+navItem[2].textContent = siteContent["nav"]["nav-item-3"];
+navItem[3].textContent = siteContent["nav"]["nav-item-4"];
+navItem[4].textContent = siteContent["nav"]["nav-item-5"];
+navItem[5].textContent = siteContent["nav"]["nav-item-6"];
+
+
+
+const middleimg = document.querySelector(".main-content img");
+middleimg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
